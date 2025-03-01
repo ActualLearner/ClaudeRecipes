d9aaec9ea39d0bf53236ce73d3ba4f8296d0167b
ActualLearner <abdellahshafi7@gmail.com>
2025-03-01 11:21:45 +0300

    Initial Project Setup

---
+++ src/Header.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Header() {
+  return (
+    <header>
+        <img src="./assets/chef-claude-icon.png" alt="" />
+    </header>
+  )
+}
+
+export default Header
